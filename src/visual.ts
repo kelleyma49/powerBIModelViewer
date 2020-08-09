8b03b332acef5c81c4388803099df2f6bd78c369
Michael Kelley <michael.a.kelley@gmail.com>
2020-08-09 08:28:53 -0700

    It's alive. IT'S ALIVE!

---
+++ src/visual.ts
@@ -0,0 +1,117 @@
+/*
+*  Power BI Visual CLI
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+"use strict";
+
+
+import 'regenerator-runtime';
+
+import "core-js/stable";
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+
+import {ModelViewerElement} from '@google/model-viewer/dist/model-viewer';
+//import ModelViewerElement from '@google/model-viewer';
+
+import { VisualSettings } from "./settings";
+export class Visual implements IVisual {
+    private target: HTMLElement;
+    private div: HTMLDivElement;
+    private modelViewer: ModelViewerElement;
+    private settings: VisualSettings;
+
+    private updateCount: number;
+    /*private settings: VisualSettings;*/
+    private textNode: Text;
+
+    constructor(options: VisualConstructorOptions) {
+        this.updateCount = 0;
+        console.log('Visual constructor', options);
+        this.target = options.element;
+        //if (document) {
+           
+
+            /*const new_p: HTMLElement = document.createElement("p");
+            new_p.appendChild(document.createTextNode("Update count:"));
+            const new_em: HTMLElement = document.createElement("em");
+            this.textNode = document.createTextNode(this.updateCount.toString());
+            new_em.appendChild(this.textNode);
+            new_p.appendChild(new_em);
+            this.target.appendChild(new_p);*/
+        //}
+    }
+
+    @logExceptions()
+    public update(options: VisualUpdateOptions) {
+        console.log("in update model viewer");
+        if (this.modelViewer) {
+            return
+        }
+
+        this.modelViewer = new ModelViewerElement();
+        //this.modelViewer.style.backgroundColor = "red";
+        this.modelViewer.src = "https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948";
+        //this.modelViewer.src = "shared-assets/models/Astronaut.glb";
+        this.modelViewer.cameraControls = true;
+        this.target.appendChild(this.modelViewer);
+
+        console.log("after model viewer");
+    }
+
+    private static parseSettings(dataView: DataView): VisualSettings {
+        return <VisualSettings>VisualSettings.parse(dataView);
+    }
+
+    /**
+     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
+     * objects and properties you want to expose to the users in the property pane.
+     *
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+    }
+}
+
+export function logExceptions(): MethodDecorator {
+    return function (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> {
+        return {
+            value: function () {
+                try {
+                    return descriptor.value.apply(this, arguments);
+                } catch (e) {
+                    console.error(e);
+                    throw e;
+                }
+            }
+        }
+    }
+}
